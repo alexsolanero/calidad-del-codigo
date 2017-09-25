ba265fb5724517bbd7e4a760cd8a147c87bacd39
alexsolanero <alex_solanero@hotmail.com>
2017-09-25 20:31:19 +0200

    Update Bu.java

--- Bu.java
+++ Bu.java
@@ -1,13 +1,30 @@
 package TrabajoProcesos;
 
-public class Bu {
+public class Bucle {
 	public static void main(String [] Args){
 		int notatrabajo=0;
 		if (notatrabajo==0){
 			System.out.println("Suspenso");
 		}else if(notatrabajo==1){
 			System.out.println("Suspenso");
-		}
+		}else if(notatrabajo==2){
+			System.out.println("Suspenso");
+		}else if(notatrabajo==3){
+			System.out.println("Suspenso");
+		}else if(notatrabajo==4){
+			System.out.println("Suspenso");
+		}else if(notatrabajo==5){
+			System.out.println("Aprobado");
+		}else if(notatrabajo==6){
+			System.out.println("Aprobado");
+		}else if(notatrabajo==7){
+			System.out.println("Aprobado");
+		}else if(notatrabajo==8){
+			System.out.println("Aprobado");
+		}else if(notatrabajo==9){
+			System.out.println("Aprobado");
+		}else if(notatrabajo==10){
+			System.out.println("Aprobado");
 		}
 	}
 }
