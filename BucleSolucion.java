64e016eceb7fa7c96bba422500fb21ef0b22dd83
alexsolanero <alex_solanero@hotmail.com>
2017-09-25 19:56:00 +0200

    Create BucleSolucion.java

---
+++ BucleSolucion.java
@@ -0,0 +1,42 @@
+package Solucion;
+
+public class BucleSolucion {
+	public static void main(String [] Args){
+		int notatrabajo=0;
+		switch(notatrabajo){
+		case 0:
+			System.out.println("Suspenso");
+			break;
+		case 1:
+			System.out.println("Suspenso");
+			break;
+		case 2:
+			System.out.println("Suspenso");
+			break;
+		case 3:
+			System.out.println("Suspenso");
+			break;
+		case 4:
+			System.out.println("Suspenso");
+			break;
+		case 5:
+			System.out.println("Aprobado");
+			break;
+		case 6:
+			System.out.println("Aprobado");
+			break;
+		case 7:
+			System.out.println("Aprobado");
+			break;
+		case 8:
+			System.out.println("Aprobado");
+			break;
+		case 9:
+			System.out.println("Aprobado");
+			break;
+		case 10:
+			System.out.println("Aprobado");
+			break;
+		}
+	}
+}
