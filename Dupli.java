6dca52fd694813c1d981af91964384268081eb0f
alexsolanero <alex_solanero@hotmail.com>
2017-09-26 14:17:36 +0200

    Create Dupli.java

---
+++ Dupli.java
@@ -0,0 +1,24 @@
+
+
+public class Dupli {
+public  static void  main(String [] Args){
+	suma1();
+	suma2();
+}
+public static void suma1(){
+	int sumar=0;
+	if(sumar>=0){
+		for(int i=0;i<5;i++){
+			sumar++;
+		}
+	}
+}
+public static void suma2(){
+	int sumar=0;
+	if(sumar>=0){
+		for(int i=0;i<5;i++){
+			sumar++;
+		}
+	}
+}
+}
