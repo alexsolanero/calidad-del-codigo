75037b3a1e5e67a6d889e55ff9146a09d5bc9968
alexsolanero <alex_solanero@hotmail.com>
2017-09-26 12:15:25 +0200

    Create duplica.java

---
+++ duplica.java
@@ -0,0 +1,34 @@
+package duplicaciones;
+
+public class duplica {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int duplicacion=0;
+	
+		
+		for(int a=0;a<20;a++){
+			duplicacion++;
+	    }
+		for(int b=0;b<20;b++){
+			duplicacion++;
+		}
+		for(int c=0;c<20;c++){
+			duplicacion++;
+		}
+		for(int d=0;d<20;d++){
+			duplicacion++;
+		}
+		for(int e=0;e<20;e++){
+			duplicacion++;
+		}
+		for(int f=0;f<20;f++){
+			duplicacion++;
+		}
+		for(int g=0;g<20;g++){
+		}
+
+}
+}
