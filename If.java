c267247ff9dc5b3586d315a23ad0134a6bc6da73
alexsolanero <alex_solanero@hotmail.com>
2017-09-30 21:27:57 +0200

    Create If.java

---
+++ If.java
@@ -0,0 +1,16 @@
+public class If{
+public static void main(String []args){
+  int i=0;
+  int j=1;
+  int k=2;
+  int f=20;
+  
+while(i==0){
+  if(k>j){
+    if(j<f){
+     k++;
+        }
+       }
+     j++;
+     }
+    }
